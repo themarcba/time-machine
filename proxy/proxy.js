a13579a430c47cf87430ad18a04df53496a10918
Marc Backes <marc.backes@talkevent.de>
2024-03-01 10:21:41 +0100

    changed js file connect

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -1,1 +1,18 @@
 console.log("Hello DevWorld 👋");
+
+const product = { price: 6, quantity: 2 };
+const proxy = new Proxy(product, {
+  get(target, property) {
+    console.log("🔎 get", property);
+    return target[property];
+  },
+
+  set(target, property, value) {
+    console.log("🔥 set", property, value);
+    target[property] = value;
+  },
+});
+
+console.log("💰", proxy.price);
+proxy.price = 999;
+console.log("💰", proxy.price);
